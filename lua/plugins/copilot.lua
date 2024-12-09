8352b202e630787ab7362f0d4cf8a662ba18009a
Will Hinson <wmhcell@gmail.com>
2024-12-09 14:42:43 -0500

    Updated ``copilot.lua`` config

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,6 +3,14 @@ return {
   cmd = "Copilot",
   event = "InsertEnter",
   config = function()
-    require("copilot").setup({})
+    require("copilot").setup({
+      suggestion = {
+        auto_trigger = true,
+        keymap = {
+          accept = "<Tab>",
+          dismiss = "<Esc>"
+        }
+      }
+    })
   end,
 }
