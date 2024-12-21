7548037c69914fb0eb015f0caf9a2c8bf154c556
Will Hinson <wmhcell@gmail.com>
2024-12-21 10:38:07 -0500

    Improved config for SQL

--- init.lua
+++ init.lua
@@ -46,6 +46,9 @@ vim.api.nvim_set_hl(0, 'yamlBlockMappingKey', {fg = '#81a1c1'})
 vim.api.nvim_set_hl(0, 'yamlBlockString', {fg = '#A3BE8C'})
 vim.api.nvim_set_hl(0, 'yamlPlainScalar', {fg = '#A3BE8C'})
 
+-- set up colors for sql
+vim.api.nvim_set_hl(0, 'sqlKeyword', {link = 'Statement'})
+
 require("notify").setup({
   background_colour = "#000000",
 })
@@ -194,3 +197,7 @@ if vim.uv.os_uname().sysname == 'Windows_NT' then
   vim.keymap.set('', '<M-t>', function() vim.cmd[[ToggleTerm horizontal split]] end, {})
   vim.keymap.set('t', '<M-t>', function() vim.cmd[[ToggleTerm horizontal split]] end, {})
 end
+
+-- set up indentation for sql
+vim.cmd('autocmd FileType sql setlocal shiftwidth=4')
+vim.cmd('autocmd FileType sql setlocal tabstop=4')
