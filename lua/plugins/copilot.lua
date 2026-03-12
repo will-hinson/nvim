f0c998f104a7679f1e45016b4f3edb84264824a3
Will Hinson <wmhcell@gmail.com>
2026-03-12 13:43:57 -0400

    Added leader bindings for enabling/disabling suggestions

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -12,5 +12,17 @@ return {
         }
       }
     })
+
+    vim.keymap.set("n", "<leader>se", function()
+      vim.cmd("Copilot enable")
+    end, { desc = "Copilot: Enable" })
+
+    vim.keymap.set("n", "<leader>sd", function()
+      vim.cmd("Copilot disable")
+    end, { desc = "Copilot: Disable" })
+
+    vim.keymap.set("n", "<leader>st", function()
+      vim.cmd("Copilot toggle")
+    end, { desc = "Copilot: Toggle"})
   end,
 }
